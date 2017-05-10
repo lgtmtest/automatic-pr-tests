93f7a1be4bdd207012d4c0fe0589aca5aee50b23
Chris Gavin <chris@chrisgavin.me>
2017-05-10 11:17:00 +0100

    Add the test code.

---
+++ javascript/existing-unchanged.js
@@ -0,0 +1,4 @@
+// Just some valid comments.
+// Some more valid comments.
+var x = 1, x = 2;
+// And another comment.
---
+++ python/existing-changed.py
@@ -0,0 +1,5 @@
+# Just a comment line.
+# Another comment line.
+x = 1
+x = 2
+# What do you know, another comment.
